aa6ece1cce7cf9aa7c3fd9775ac15907ce979b77
Raja Ayyagari <raja@umd.edu>
2014-08-19 00:04:22 -0400

    remove todos

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,4 +10,11 @@
 angular.module('todoApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = ['Item 1','Item 2','Item 3','Item 4'];
+    $scope.addTodo = function(){
+  	  $scope.todos.push($scope.todo);
+  	  $scope.todo = '';
+    };
+    $scope.removeTodo = function (index) {
+  	  $scope.todos.splice(index, 1);
+	};
   });
